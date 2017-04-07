c3801a7ecadb15906a16f68b4b41c92ee3e3cf1b
Jean Carlos Danese <jean@danese.com.br>
2017-04-07 14:31:39 -0300

    Calculando...

--- src/main/java/io/hyberika/CurriculoController.java
+++ src/main/java/io/hyberika/CurriculoController.java
@@ -1,10 +1,15 @@
 package io.hyberika;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CurriculoController {
+	
+	private Integer pontos = 0;
 	
 	@RequestMapping("/")
 	public String index(){
@@ -14,6 +19,21 @@ public class CurriculoController {
 	@RequestMapping("questionario")
 	public String questionario(){
 		return "questionario";
+	}
+	
+	@RequestMapping(value = "enviar", method = RequestMethod.POST )
+	public String salvar(
+			@RequestParam("nome") String nome,
+			@RequestParam("quest-1") String quest1, Model model){
+		
+		if(quest1.equals("true")) {
+			pontos++;
+		}
+		
+		model.addAttribute("pontos", pontos);
+		
+		return "resultado";
+		
 	}
 
 }
